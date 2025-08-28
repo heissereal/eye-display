380b4f8e482cf1194d0306e82eec4abd6f6513d1
heissereal <miyamichi@jsk.imi.i.u-tokyo.ac.jp>
2025-08-28 22:29:12 +0800

    add publisher of vad values

--- simple_version/bin/send_eye_status.py
+++ simple_version/bin/send_eye_status.py
@@ -9,6 +9,7 @@ import rospy
 from i2c_for_esp32 import WirePacker  # pip3 install i2c-for-esp32
 from std_msgs.msg import UInt16
 from std_msgs.msg import Float32
+from std_msgs.msg import Float32MultiArray
 
 I2C_SLAVE = 0x0703
 
@@ -46,11 +47,19 @@ class I2C:
 
 
 i2c_right = I2C(device=0x42,bus=0)
-i2c_left = I2C(device=0x43,bus=5)
+i2c_left = I2C(device=0x42,bus=5)
 
 eye_status = 0
 look_at = 0.0
-switch_flag = True
+v = 0.0
+a = 0.0
+d = 0.0
+
+# lid_y = 0.0
+# lid_d = 0.0  
+# iris_v = 0.0
+# changer_v = 0.0
+# switch_flag = True
 
 def send_eye_status(data):
    sent_str = str(data)
@@ -67,21 +76,54 @@ def send_eye_status(data):
       except OSError as e:
          print(e)
 
+# def sub_lid_y_cb(msg):
+#    global lid_y
+#    lid_y = msg.data
+
+# def sub_lid_d_cb(msg):
+#    global lid_d
+#    lid_d = msg.data
+
+# def sub_iris_v_cb(msg):
+#    global iris_v
+#    iris_v = msg.data
+
+# def sub_changer_v_cb(msg):
+#     global changer_v
+#     changer_v = msg.data
 
 def sub_eye_status_cb(msg):
-   global eye_status, switch_flag
+   global eye_status
    eye_status = msg.data
-   if eye_status > 0:
-      switch_flag = True
-      if eye_status == 7:
-         switch_flag = False
+   # switch_flag = False
+#    if eye_status > 0:
+#       switch_flag = True
+#       if eye_status == 7:
+#          switch_flag = False
 
 
+def sub_vad_cb(msg):
+   global v, a, d
+   if len(msg.data) >= 3:
+       v = msg.data[0]
+       a = msg.data[1]
+       d = msg.data[2]
+        
 def eye(event):
-   if switch_flag:
-      send_eye_status(eye_status)
-   else:
-      send_eye_status(look_at)
+   send_eye_status(f"V{v:.2f}")
+   send_eye_status(f"A{a:.2f}")
+   send_eye_status(f"D{d:.2f}")
+   send_eye_status(f"X{look_at:.2f}")
+   # print(switch_flag)
+   # if switch_flag:
+   #    send_eye_status(f"S{eye_status}")
+   # else:
+   #    # send_eye_status(f"X{look_at:.2f}")
+   #    send_eye_status(f"L{lid_y:.2f}")
+   #    send_eye_status(f"D{lid_d:.2f}")
+   #    send_eye_status(f"V{iris_v:.2f}")
+   #    send_eye_status(f"C{changer_v:.2f}")
+            
 
 def sub_look_at_cb(msg):
    global look_at
@@ -95,6 +137,11 @@ if __name__ == '__main__':
    rospy.init_node('eye_status_to_I2C')
    rospy.Subscriber('/eye_status',UInt16,sub_eye_status_cb)
    rospy.Subscriber('/look_at',Float32,sub_look_at_cb)
+   # rospy.Subscriber("lid_y", Float32, sub_lid_y_cb)
+   # rospy.Subscriber('/lid_d', Float32, sub_lid_d_cb)
+   # rospy.Subscriber('/iris_v', Float32, sub_iris_v_cb)
+   # rospy.Subscriber("changer_v", Float32, sub_changer_v_cb)
+   rospy.Subscriber('/vad', Float32MultiArray, sub_vad_cb) 
    rospy.Timer(rospy.Duration(0.1), eye)
    rospy.spin()
 
